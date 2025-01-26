30661d7a21253dfca53b4ec32ece51720779936e
gtaexecutiverentals <gtaexecutiverentals@gmail.com>
2025-01-25 23:00:50 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+
+var bodyParser = require('body-parser');
+var axios = require('axios');
+var routes = require('./routes/routes.js');
+var moment = require('moment');
+var date = moment().utcOffset(-300).format('LLLL');
+var port = Number(process.env.NODEJS_SERVICE_PORT || process.env.PORT || 8080 || 3002);
+app.enable('trust proxy');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+
+   app.use(routes);
+
+app.listen(port, function(error) {
+  if(error) {
+    console.log(error);
+  }
+  console.log('Server started at ' + date + ' and running on Port: ' + port);
+});
